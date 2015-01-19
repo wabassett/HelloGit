ea01192615d4f925992f1abad4f56d25817d772d
wabassett <wabassett@gmail.com>
2015-01-19 01:53:12 -0800

    Modified HelloGit

--- HelloGit/src/HelloGit.java
+++ HelloGit/src/HelloGit.java
@@ -3,7 +3,8 @@ public class HelloGit {
 
 	public static void main(String[] args) {
 		System.out.println("Hello CSC656 Group 1!");
-		System.out.println("Will should modify the project and comment out this line!");
+//		System.out.println("Will should modify the project and comment out this line!");
+		System.out.println("Will commented out his comment line, and you should all see this instead!");
 		System.out.println("Bobby should modify the project and comment out this line!");
 		System.out.println("Warren should modify the project and comment out this line!");
 		System.out.println("Andrew should modify the project and comment out this line!");        		
